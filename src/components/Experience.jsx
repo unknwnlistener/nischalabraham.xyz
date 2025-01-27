81901d2126a20b26efdcc4c2dbf2a30c7421ea69
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2025-01-27 12:09:10 +0000

    feat: Improved Education section

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,3 +1,5 @@
+import resume from "@/assets/nischal-abraham-resume.pdf";
+
 export default function Experience() {
     const ExperienceListItem = ({
         company,
@@ -5,14 +7,43 @@ export default function Experience() {
         startDate,
         endDate,
         description,
-        techStack,
+        skillsList,
     }) => {
+        const getMonthYear = (date) => {
+            const dateObj = new Date(date);
+            return `${dateObj.toLocaleDateString(dateObj, { month: "long", year: "numeric" })}`;
+        };
+        const getDateString = (date) => {
+            return new Date(date).toISOString().split("T")[0];
+        };
+
+        const getSkillsAsList = () => {
+            return skillsList.split(",").map((el) => el.trim());
+        };
         return (
-            <li>
-                <p>
-                    {title} - {company}
-                </p>
-                <div>{description}</div>
+            <li className="experience-block">
+                <div>
+                    <time dateTime={getDateString(startDate)}>
+                        {getMonthYear(startDate)}
+                    </time>{" "}
+                    &mdash;{" "}
+                    <time dateTime={getDateString(endDate)}>
+                        {getMonthYear(endDate)}
+                    </time>
+                </div>
+                <section className="stack">
+                    <h3>
+                        {title} &mdash; {company}
+                    </h3>
+                    <p>{description}</p>
+                    <ul className="cluster">
+                        {getSkillsAsList().map((skill) => (
+                            <li className="pill" key={skill}>
+                                {skill}
+                            </li>
+                        ))}
+                    </ul>
+                </section>
             </li>
         );
     };
@@ -21,28 +52,35 @@ export default function Experience() {
             <h2>Experience</h2>
             <ul className="stack">
                 <ExperienceListItem
-                    company={"Postal Bible School, Ireland"}
+                    company={"Postal Bible School"}
                     title={
-                        "Information and Communications Technology (ICT)Manager"
+                        "Information and Communications Technology (ICT) Manager"
                     }
-                    startDate={"August 2022"}
-                    endDate={"December 2024"}
+                    startDate={"2022-08"}
+                    endDate={"2024-12"}
                     description={
                         "Redesigned and migrated the primary website from WordPress to React, enhancing user experience and reducing pages by 60%. Led the development of a digital learning management system (LMS), utilizing React, PHP and Moodle to streamline access for 3000+ students.  Managed cloud server deployment on Cloudways, utilizing GitHub Actions for automated CI/CD workflows."
                     }
-                    techStack={"Laravel, PHP, React, Typescript, Cypress, Jest"}
+                    skillsList={
+                        "Laravel, PHP, React, Typescript, Cypress, Jest"
+                    }
                 />
                 <ExperienceListItem
-                    company={"Media.net(Perform Media), Mumbai, India"}
+                    company={"Media.net(Perform Media)"}
                     title={"Web Application Developer"}
-                    startDate={"July 2017"}
-                    endDate={"August 2019"}
+                    startDate={"2017-07"}
+                    endDate={"2019-08"}
                     description={
                         "Improved report access time for publishers by 32% by optimizing Excel and CSV file imports, implementing custom parsing algorithms, and utilizing advanced data processing techniques. Successfully transitioned CI technology from Apache SVN to Git, leading training sessions and creating documentation that reduced collaborative errors."
                     }
-                    techStack={"HTML, CSS, JS, Angular, ASP.NET MVC, Git"}
+                    skillsList={
+                        "HTML, CSS, JavaScript, Angular, ASP.NET MVC, Git"
+                    }
                 />
             </ul>
+            <aside>
+                <a href={resume}>View Full Resume</a>
+            </aside>
         </article>
     );
 }
