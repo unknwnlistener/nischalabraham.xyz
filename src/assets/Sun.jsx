160df68b7191481697e4c77c369ec2ab906352ee
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2025-03-25 17:31:41 +0000

    feat: Implement theme toggle functionality with dark mode support

---
+++ src/assets/Sun.jsx
@@ -0,0 +1,42 @@
+export default function Sun({ darkMode }) {
+    return (
+        <svg width="30" height="30" xmlns="http://www.w3.org/2000/svg">
+            <title>Theme toggle</title>
+
+            <path
+                fill={"var(--base-color)"}
+                stroke={"var(--base-color)"}
+                fillOpacity={0.35}
+                strokeOpacity={1}
+                className="theme-toggle-icon"
+                d={
+                    darkMode
+                        ? "M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"
+                        : "M 15,15 m 8, 0 a 8,8 0 1,0 -16,0 a 8,8 0 1,0  16,0"
+                }
+                transform={darkMode ? "scale(1.3)" : "scale(1)"}
+            />
+            <g
+                stroke={darkMode ? "none" : "var(--base-color)"}
+                className="sun-rays"
+            >
+                <line
+                    id="ray"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    x1="15"
+                    y1="1"
+                    x2="15"
+                    y2="4"
+                />
+                <use href="#ray" transform="rotate(45 15 15)" />
+                <use href="#ray" transform="rotate(90 15 15)" />
+                <use href="#ray" transform="rotate(135 15 15)" />
+                <use href="#ray" transform="rotate(180 15 15)" />
+                <use href="#ray" transform="rotate(225 15 15)" />
+                <use href="#ray" transform="rotate(270 15 15)" />
+                <use href="#ray" transform="rotate(315 15 15)" />
+            </g>
+        </svg>
+    );
+}
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,10 @@
+import ThemeToggleButton from "./ThemeToggleButton";
+
 export default function Navbar() {
     return (
         <nav className="navbar">
             <img src="/nalogo.svg" alt="Website logo" />
+            <ThemeToggleButton />
         </nav>
     );
 }
