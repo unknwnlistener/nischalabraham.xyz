1bee92a455f720006653b5f631299f1fd442a6ef
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2025-06-04 14:55:07 +0100

    fix: Added titles to SVGs

--- src/assets/JavascriptLogo.jsx
+++ src/assets/JavascriptLogo.jsx
@@ -1,6 +1,7 @@
 export default function JavascriptLogo({ size = "36" }) {
     return (
         <svg width={size} height={size} viewBox="0 0 256 256" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <title>Javascript logo</title>
             <g>
                 <path d="M0,0 L256,0 L256,256 L0,256 L0,0 Z" fill="#F7DF1E">
                 </path>
