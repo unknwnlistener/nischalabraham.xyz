447d56926a9d914bfe3623390282a11531337db5
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2025-06-04 14:43:30 +0100

    feat: Fancy border on profile section

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -6,14 +6,14 @@ import MapPin from "../assets/MapPin";
 
 export default function Profile() {
     return (
-        <section className="[ center-y ][ profile ]">
+        <section className="[ center-y margin-top-4 ][ profile ]">
             <div>
                 <div className="[ flex ][ center-y gap-s ]">
                     <img src={profilePic} alt="Headshot of Nischal Abraham" className="margin-top-2" />
-                    <div>
+                    <div className="[ bg-chat point-left padding-s ]">
                         <p>Hey, I&apos;m Nischal!</p>
                         <h1>Fullstack Web Developer</h1>
-                        <p className="center-y gap-2xs"><MapPin /> Londonderry, UK</p>
+                        <p className="center-y gap-3xs"><MapPin /> Londonderry, UK</p>
                     </div>
                 </div>
                 <Socials showResume />
