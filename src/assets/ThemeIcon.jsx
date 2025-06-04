8a40286f2bfa3166c3f339e3ac86af6996635d6c
Nischal - Windows <10276779+unknwnlistener@users.noreply.github.com>
2025-06-04 11:33:59 +0100

    fix: Icon breaking for light preference on system

--- src/assets/ThemeIcon.jsx
+++ src/assets/ThemeIcon.jsx
@@ -4,20 +4,20 @@ export default function ThemeIcon({ isDark = false, isSystem = false }) {
             <title>{isSystem ? "System" : isDark ? "Dark" : "Light"}</title>
 
             <path
-                fill={"var(--foreground-color)"}
-                stroke={"var(--foreground-color)"}
+                fill={"var(--clr-fg)"}
+                stroke={"var(--clr-fg)"}
                 fillOpacity={0.35}
                 strokeOpacity={1}
                 className="theme-toggle-icon"
                 d={
-                    isDark
+                    (isSystem || isDark)
                         ? "M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"
                         : "M 15,15 m 8, 0 a 8,8 0 1,0 -16,0 a 8,8 0 1,0  16,0"
                 }
                 transform={isSystem ? "scale(0.9) translate(4 4)" : (isDark ? "scale(1.3)" : "scale(1)")}
             />
             <g
-                stroke={(!isSystem && isDark) ? "none" : "var(--foreground-color)"}
+                stroke={(!isSystem && isDark) ? "none" : "var(--clr-fg)"}
                 className="sun-rays"
             >
                 <line
